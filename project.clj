0b35f1aa1269c50ff311989f25e5dc1f1467ae55
Michael S. Klishin <michael@novemberain.com>
2011-09-14 14:52:56 +0400

    Use Clojure 1.3.0-RC0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.novemberain/pantomime "0.1.0-SNAPSHOT"
   :description        "A tiny standalone library that deals with MIME types"
   :license            { :name "Eclipse Public License" }
-  :dependencies       [[org.clojure/clojure "1.3.0-beta3"]]
+  :dependencies       [[org.clojure/clojure "1.3.0-RC0"]]
   :dev-resources-path "test/resources")
