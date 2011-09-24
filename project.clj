8c9e91860c12fde1b825d65fdd5e1ef444d72936
Michael S. Klishin <michael@novemberain.com>
2011-09-24 13:28:43 +0400

    Clojure 1.3.0 is out

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.novemberain/pantomime "0.1.0-SNAPSHOT"
   :description        "A tiny standalone library that deals with MIME types"
   :license            { :name "Eclipse Public License" }
-  :dependencies       [[org.clojure/clojure "1.3.0-RC0"]]
+  :dependencies       [[org.clojure/clojure "1.3.0"]]
   :dev-resources-path "test/resources")
