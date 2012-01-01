8e15b0be247d14a477d0a38f099cebd058118e09
Michael S. Klishin <michael@novemberain.com>
2012-01-01 12:35:28 +0400

    Update artifact description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.novemberain/pantomime "1.1.0-SNAPSHOT"
-  :description        "A tiny standalone library that deals with MIME types"
+  :description        "A tiny Clojure library that deals with MIME types"
   :license            { :name "Eclipse Public License" }
   :dependencies       [[org.clojure/clojure       "1.3.0"]
                        [org.apache.tika/tika-core "1.0"]]
