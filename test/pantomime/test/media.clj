3598aa3c110e3eb38d64ade37204fa8f15b7b573
Michael S. Klishin <michael@novemberain.com>
2012-02-09 14:32:20 +0400

    Demonstrate that pantomime.media/media-type-named is case-insensitive

--- test/pantomime/test/media.clj
+++ test/pantomime/test/media.clj
@@ -6,5 +6,6 @@
 (deftest test-media-type-named
   (is (= media/TEXT_PLAIN       (media/media-type-named "text/plain")))
   (is (= media/TEXT_HTML        (media/media-type-named "text/html")))
+  (is (= media/TEXT_HTML        (media/media-type-named "text/HTML")))
   (is (= media/APPLICATION_XML  (media/media-type-named "application/xml")))
   (is (= media/APPLICATION_JSON (media/media-type-named "application/json"))))
