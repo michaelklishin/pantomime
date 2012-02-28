860eb01cf049b8748a6d4901e62d676bb2f54011
Michael S. Klishin <michael@novemberain.com>
2012-02-28 19:18:26 +0400

    Now working on 1.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.novemberain/pantomime "1.1.0"
+(defproject com.novemberain/pantomime "1.2.0-SNAPSHOT"
   :description        "A tiny Clojure library that deals with MIME types"
   :license            { :name "Eclipse Public License" }
   :dependencies       [[org.clojure/clojure       "1.3.0"]
