308a3efa55c13bff437354209ab2cedbf0e4f6c0
Michael S. Klishin <michael@novemberain.com>
2012-03-13 22:00:54 +0400

    Use Leiningen 2 aliases

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license { :name "Eclipse Public License" }
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.apache.tika/tika-core "1.0"]]
-  :profiles {:all { :dependencies [[org.apache.tika/tika-core "1.0"]] }
-             :dev { :resource-paths ["test/resources"]}
-             :1.4 { :resource-paths ["test/resources"]
-                    :dependencies [[org.clojure/clojure "1.4.0-beta4"]] }}
+  :profiles {:dev { :resource-paths ["test/resources"]}
+             :1.4 { :dependencies [[org.clojure/clojure "1.4.0-beta4"]] }}
+  :aliases  { "all" ["with-profile" "dev:dev,1.4"] }
   :warn-on-reflection true)
