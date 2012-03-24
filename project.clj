5c54c84fbb1e0b1ae5b201b111ee14420e809ea3
Michael S. Klishin <michael@novemberain.com>
2012-03-25 01:25:44 +0400

    Upgrade to Apache Tika 1.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :description "A tiny Clojure library that deals with MIME types"  
   :license { :name "Eclipse Public License" }
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache.tika/tika-core "1.0"]]
+                 [org.apache.tika/tika-core "1.1"]]
   :profiles {:dev { :resource-paths ["test/resources"]}
              :1.4 { :dependencies [[org.clojure/clojure "1.4.0-beta4"]] }}
   :aliases  { "all" ["with-profile" "dev:dev,1.4"] }
