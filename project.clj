372e7a87e7193b44777ffdd99a956ec2455af2d2
Michael S. Klishin <michael@novemberain.com>
2012-04-07 23:25:06 +0400

    1.3.0-beta2

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.novemberain/pantomime "1.3.0-SNAPSHOT"
+(defproject com.novemberain/pantomime "1.3.0-beta2"
   :min-lein-version "2.0.0"
   :description "A tiny Clojure library that deals with MIME types"  
   :license { :name "Eclipse Public License" }
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.apache.tika/tika-core "1.1"]]
   :profiles {:dev { :resource-paths ["test/resources"]}
-             :1.4 { :dependencies [[org.clojure/clojure "1.4.0-beta4"]] }}
+             :1.4 { :dependencies [[org.clojure/clojure "1.4.0-beta6"]] }}
   :aliases  { "all" ["with-profile" "dev:dev,1.4"] }
   :warn-on-reflection true)
