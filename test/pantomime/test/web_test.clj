31d2344a421404af719aed10f72ddb181303d88f
Michael S. Klishin <michael@novemberain.com>
2012-04-12 01:59:24 +0400

    One more test case

--- test/pantomime/test/web_test.clj
+++ test/pantomime/test/web_test.clj
@@ -17,4 +17,5 @@
        "http://docs.oracle.com/javase/7/docs/index.html"                     "application/xhtml+xml"
        "http://upload.wikimedia.org/wikipedia/en/1/1a/Clojure-glyph.svg"     "image/svg+xml"
        "http://upload.wikimedia.org/wikipedia/commons/9/9a/PNG_transparency_demonstration_2.png" "image/png"
-       "http://creativecommons.org/images/deed/cc-logo.jpg" "image/jpeg"))
+       "http://creativecommons.org/images/deed/cc-logo.jpg" "image/jpeg"
+       "http://elpais.com.uy/formatos/rss/index.asp?seccion=umomento" "application/rss+xml"))
