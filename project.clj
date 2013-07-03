4b0bf179744411aea7870cb756e659f10fee3e60
Michael Klishin <michael@defprotocol.org>
2013-07-03 23:45:41 +0400

    Upgrade clj-http dev dependency

--- project.clj
+++ project.clj
@@ -6,8 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.apache.tika/tika-core "1.4"]]
   :profiles {:dev {:resource-paths ["test/resources"]
-                   :dependencies [[clj-http "0.6.4"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+                   :dependencies [[clj-http "0.7.4"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
