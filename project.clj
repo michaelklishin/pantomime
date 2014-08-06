382006b0c1855fc7fe76f5340541fb56a3cc02e6
Michael Klishin <michael@clojurewerkz.org>
2014-08-06 05:40:01 +0400

    Bump Leiningen requirement to 2.4.3 (uses HTTPS for Maven Central)

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.novemberain/pantomime "2.4.0-SNAPSHOT"
-  :min-lein-version "2.4.2"
+  :min-lein-version "2.4.3"
   :description "A tiny Clojure library that deals with MIME types"
   :license { :name "Eclipse Public License" }
   :source-paths ["src/clojure"]
