adf7434a1ae53a7d3c5fae10487959c95847fa81
Michael Klishin <michael@clojurewerkz.org>
2014-09-18 08:04:44 +0400

    Bump Leiningen requirement to 2.5.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.novemberain/pantomime "2.4.0-SNAPSHOT"
-  :min-lein-version "2.4.3"
+  :min-lein-version "2.5.0"
   :description "A tiny Clojure library that deals with MIME types"
   :license { :name "Eclipse Public License" }
   :source-paths ["src/clojure"]
