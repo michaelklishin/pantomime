0677ea05242e61e4c4e1e87e8d57daeb3c0924db
Michael Klishin <michael@clojurewerkz.org>
2014-11-22 18:23:28 +0300

    Drop Clojure 1.4

--- project.clj
+++ project.clj
@@ -7,7 +7,6 @@
                  [org.apache.tika/tika-core "1.5"]]
   :profiles {:dev {:resource-paths ["test/resources"]
                    :dependencies [[clj-http "0.9.1"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
@@ -16,5 +15,5 @@
                  "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}}
-  :aliases  {"all" ["with-profile" "+dev:+1.4:+1.5:+master"]}
+  :aliases  {"all" ["with-profile" "+dev:+1.5:+master"]}
   :global-vars {*warn-on-reflection* true})
