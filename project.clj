fc59fb660924b675dd4adfe9f29ad71d0f12a346
Michael Klishin <michael@clojurewerkz.org>
2017-02-02 10:36:48 +0300

    Depend on Clojure 1.8

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :url "http://github.com/michaelklishin/pantomime"
   :license { :name "Eclipse Public License" }
   :source-paths ["src/clojure"]
-  :dependencies [[org.clojure/clojure "1.7.0"]                 
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.apache.tika/tika-parsers "1.14"]]
   :profiles {:dev {:resource-paths ["test/resources"]
                    :dependencies [[clj-http "2.0.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :master {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
@@ -16,5 +16,5 @@
                  "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}}
-  :aliases  {"all" ["with-profile" "+dev:+1.8:+master"]}
+  :aliases  {"all" ["with-profile" "+dev:+1.7:+master"]}
   :global-vars {*warn-on-reflection* true})
